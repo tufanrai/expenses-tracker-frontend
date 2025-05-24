3aa57337786ef087229e5d6dcb9bb0d704a935ca
TufanRai <thulungtufan16@gmail.com>
2025-05-25 00:18:35 +0545

    added varcel element

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Layout from "@/component/form/layout";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SpeedInsights />
         <Layout>{children}</Layout>
       </body>
     </html>
